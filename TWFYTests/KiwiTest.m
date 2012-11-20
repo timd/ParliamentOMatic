144211d1f96876da9a56a48314a19430215d2ee5
Tim Duckett <tim@timduckett.co.uk>
2012-11-20 12:36:02 +0000

    Added in Kiwi tests

---
+++ TWFYTests/KiwiTest.m
@@ -0,0 +1,21 @@
+//
+//  KiwiTest.m
+//  TWFY
+//
+//  Created by Tim on 20/11/2012.
+//  Copyright (c) 2012 Charismatic Megafauna Ltd. All rights reserved.
+//
+
+#import "Kiwi.h"
+
+SPEC_BEGIN(MathSpec)
+
+describe(@"Math", ^{
+    it(@"is pretty cool", ^{
+        NSUInteger a = 16;
+        NSUInteger b = 26;
+        [[theValue(a + b) should] equal:theValue(42)];
+    });
+});
+
+SPEC_END
